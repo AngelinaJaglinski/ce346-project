74d6c6f47a11f9268078f242aad0c3d213dc1c87
angelinajaglinski <angelinajaglinski2023@u.northwestern.edu>
2021-11-18 13:31:59 -0800

    test

--- software/apps/pwm_tone/main.c
+++ software/apps/pwm_tone/main.c
@@ -11,6 +11,7 @@
 #include "nrfx_pwm.h"
 
 #include "microbit_v2.h"
+#include "gpio.h"
 
 // PWM configuration
 static const nrfx_pwm_t PWM_INST = NRFX_PWM_INSTANCE(0);
@@ -57,28 +58,83 @@ static void play_tone(uint16_t frequency) {
   nrfx_pwm_simple_playback(&PWM_INST, &pwm_sequence, 1, NRFX_PWM_FLAG_LOOP);
 }
 
+uint16_t NOTES_ARRAY[] = {261.6256, 261.6256, 391.9954, 391.9954, 440, 440, 391.9954};
 
+  /*
+while(1):
+  microbit plays current note
+  delay
+  if user plays current note:
+      pop current note
+  delay
+  
+
+play_next_note():
+pops first note in notes linked list
+  */
+  
 int main(void) {
   printf("Board started!\n");
 
   // initialize PWM
   pwm_init();
+  printf("%d", sizeof(NOTES_ARRAY));
 
-  // Play the A4 tone for one second
+  while(1){
+
+      if (!gpio_read(14)) {
+	gpio_set(20);
+     } else {
+      if (!gpio_read(23)) {
+	gpio_clear(20);
+	}
+     };
+      nrf_delay_ms(100);
+  }
+
+  for(int i = 0; i < sizeof(NOTES_ARRAY)/2; ++i){
+    play_tone(NOTES_ARRAY[i]);
+    nrf_delay_ms(500);
+  }
+
+  /*
+
+  // C4 (Middle C)
+  play_tone(261.6256);
+  nrf_delay_ms(1000);
+
+  // D4
+  play_tone(293.6648);
+  nrf_delay_ms(1000);
+
+  // E4
+  play_tone(329.6276);
+  nrf_delay_ms(1000);
+
+  //F4
+  play_tone(349.2282);
+  nrf_delay_ms(1000);
+
+  //G4
+  play_tone(391.9954);
+  nrf_delay_ms(1000);
+
+  //A4
   play_tone(440);
   nrf_delay_ms(1000);
 
-  // Play the C#5 tone for one second
-  play_tone(554);
+  //B4
+  play_tone(493.8833);
+  nrf_delay_ms(1000);
+  
+  //C5
+  play_tone(523.2511);
   nrf_delay_ms(1000);
 
-  // Play the E5 tone for one second
-  play_tone(659);
-  nrf_delay_ms(1000);
+  */
 
-  // Play the A5 tone for one second
-  play_tone(880);
-  nrf_delay_ms(1000);
+  
+
 
   // Stop all noises
   nrfx_pwm_stop(&PWM_INST, true);

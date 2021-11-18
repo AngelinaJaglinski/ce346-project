550c7a5c556e259274cd8965bc3c39d90be14756
AngelinaJaglinski <84103815+AngelinaJaglinski@users.noreply.github.com>
2021-11-18 14:28:32 -0600

    Update main.c

--- software/apps/led_matrix/main.c
+++ software/apps/led_matrix/main.c
@@ -11,11 +11,31 @@
 #include "led_matrix.h"
 #include "microbit_v2.h"
 
+int la1[8];
+int la2[8];
+
 int main(void) {
   printf("Board started!\n");
   
+  la1[0] = 72;
+  la1[1] = 105;
+  la1[2] = 67;
+  la1[3] = 69;
+  la1[4] = 51;
+  la1[5] = 52;
+  la1[6] = 54;
+  la1[7] = 33;
+  la2[0] = 73;
+  la2[1] = 116;
+  la2[2] = 119;
+  la2[3] = 111;
+  la2[4] = 114;
+  la2[5] = 107;
+  la2[6] = 115;
+  la2[7] = 33;
+
   // initialize LED matrix driver
-  led_matrix_init();
+  led_matrix_init(la1, la2);
 
   // call other functions here
 
@@ -24,4 +44,3 @@ int main(void) {
     nrf_delay_ms(1000);
   }
 }
-
